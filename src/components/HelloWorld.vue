48adff8272e812a6ee1ec02c7a287845d935dd13
wujian <406635565@qq.com>
2019-07-18 17:00:41 +0800

    add element

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,18 @@
 <template>
-  <div></div>
+  <div>
+    <el-switch v-model="value" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-    }
+      value:true
+    };
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
